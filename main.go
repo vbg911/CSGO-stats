85cc50f80f8ceca15c8da14717a003e3b34be6db
vbg911 <@vbg911@gmail.com>
2023-07-24 20:01:46 +0300

    Added generating of heatmap function

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/golang/geo/r2"
+	ex "github.com/markus-wa/demoinfocs-golang/v3/examples"
 	dem "github.com/markus-wa/demoinfocs-golang/v3/pkg/demoinfocs"
 	"github.com/markus-wa/demoinfocs-golang/v3/pkg/demoinfocs/common"
 	events "github.com/markus-wa/demoinfocs-golang/v3/pkg/demoinfocs/events"
+	"github.com/markus-wa/demoinfocs-golang/v3/pkg/demoinfocs/msg"
+	heatmap "github.com/markus-wa/go-heatmap/v2"
+	schemes "github.com/markus-wa/go-heatmap/v2/schemes"
+	"image"
+	"image/draw"
+	"image/jpeg"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type playersFootSteps map[uint64]int
@@ -15,6 +24,12 @@ type playersDuckKills map[uint64]int
 type playersFlashedKills map[uint64]int
 type playersAirborneKills map[uint64]int
 type playersWallbangKills map[uint64]int
+
+const (
+	dotSize     = 15
+	opacity     = 128
+	jpegQuality = 100
+)
 
 func main() {
 	entries, err := os.ReadDir("./demos")
@@ -33,6 +48,9 @@ func main() {
 		p := dem.NewParser(f)
 		defer p.Close()
 
+		header, err := p.ParseHeader()
+		checkError(err)
+
 		playersFootStep := make(map[uint64]int, 10)
 		playersDuckKill := make(map[uint64]int, 10)
 		playersFlashedKill := make(map[uint64]int, 10)
@@ -43,6 +61,33 @@ func main() {
 			handleKill(e, playersDuckKill, playersFlashedKill, playersAirborneKill, playersWallbangKill)
 		})
 		p.RegisterEventHandler(func(e events.RoundStart) {})
+
+		var (
+			mapMetadata ex.Map
+			mapRadarImg image.Image
+		)
+
+		p.RegisterNetMessageHandler(func(msg *msg.CSVCMsg_ServerInfo) {
+			// Get metadata for the map that the game was played on for coordinate translations
+			mapMetadata = ex.GetMapMetadata(header.MapName, msg.GetMapCrc())
+
+			// Load map overview image
+			mapRadarImg = ex.GetMapRadar(header.MapName, msg.GetMapCrc())
+		})
+
+		// Register handler for WeaponFire, triggered every time a shot is fired
+		var points []r2.Point
+
+		p.RegisterEventHandler(func(e events.WeaponFire) {
+			// Translate positions from in-game coordinates to radar overview image pixels
+			x, y := mapMetadata.TranslateScale(e.Shooter.Position().X, e.Shooter.Position().Y)
+
+			points = append(points, r2.Point{X: x, Y: y})
+		})
+
+		p.RegisterEventHandler(func(events.DataTablesParsed) {
+
+		})
 
 		err = p.ParseToEnd()
 		if err != nil {
@@ -60,6 +105,8 @@ func main() {
 		for _, player := range stats {
 			fmt.Println(player.formatString() + "\n")
 		}
+		name, _ := strings.CutSuffix(e.Name(), ".dem")
+		generateHeatMap(points, mapRadarImg, name+".jpeg")
 		println("\n")
 	}
 }
@@ -135,5 +182,44 @@ func statsFor(p *common.Player, fs playersFootSteps, dk playersDuckKills, fk pla
 		FlashedKills:  fk[p.SteamID64],
 		AirborneKills: airk[p.SteamID64],
 		WallbangKills: wbk[p.SteamID64],
+	}
+}
+
+func generateHeatMap(points []r2.Point, mapRadarImg image.Image, name string) {
+	r2Bounds := r2.RectFromPoints(points...)
+	padding := float64(dotSize) / 2.0 // Calculating padding amount to avoid shrinkage by the heatmap library
+	bounds := image.Rectangle{
+		Min: image.Point{X: int(r2Bounds.X.Lo - padding), Y: int(r2Bounds.Y.Lo - padding)},
+		Max: image.Point{X: int(r2Bounds.X.Hi + padding), Y: int(r2Bounds.Y.Hi + padding)},
+	}
+
+	// Transform r2.Points into heatmap.DataPoints
+	data := make([]heatmap.DataPoint, 0, len(points))
+
+	for _, p := range points[1:] {
+		// Invert Y since go-heatmap expects data to be ordered from bottom to top
+		data = append(data, heatmap.P(p.X, p.Y*-1))
+	}
+
+	//
+	// Drawing the image
+	//
+
+	// Create output canvas and use map overview image as base
+	img := image.NewRGBA(mapRadarImg.Bounds())
+	draw.Draw(img, mapRadarImg.Bounds(), mapRadarImg, image.Point{}, draw.Over)
+
+	// Generate and draw heatmap overlay on top of the overview
+	imgHeatmap := heatmap.Heatmap(image.Rect(0, 0, bounds.Dx(), bounds.Dy()), data, dotSize, opacity, schemes.AlphaFire)
+	draw.Draw(img, bounds, imgHeatmap, image.Point{}, draw.Over)
+	f, err := os.Create("img/AlphaFire_" + name)
+	// Write to stdout
+	err = jpeg.Encode(f, img, &jpeg.Options{Quality: jpegQuality})
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
